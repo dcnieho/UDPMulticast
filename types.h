c986f9f57c84f6fbe62fa6434db98b8a94260d84
Diederick Niehorster <dcnieho@gmail.com>
2015-11-05 09:50:54 +0100

    message struct needed comparison operator for boost.python vector wrapper support

--- types.h
+++ types.h
@@ -3,6 +3,7 @@
 #include "mpmcBoundedQueue.h"
 
 #include <deque>
+#include <cstring>
 
 struct EXTENDED_OVERLAPPED : public OVERLAPPED
 {
@@ -19,6 +20,13 @@ struct message
 	char* text;
 	int64_t timeStamp;
 	// source ip
+
+	// need comparison operator for boost.python objects contained in vectors
+	// see http://stackoverflow.com/questions/10680691/why-do-i-need-comparison-operators-in-boost-python-vector-indexing-suite
+	bool message::operator ==(const message &b) const
+	{
+		return timeStamp == b.timeStamp && strcmp(text, b.text);
+	};
 };
 template <size_t buffer_size>
 using queue_t = mpmc_bounded_queue<message,buffer_size>;
