92a857714c55e40c52aca483f584318c6595b58f
Diederick Niehorster <dcnieho@gmail.com>
2015-11-05 11:24:57 +0100

    added python message vector todo: named tuple?

--- UDPClient_python/UDPClient_python.cpp
+++ UDPClient_python/UDPClient_python.cpp
@@ -12,6 +12,12 @@ using namespace boost::python;
 BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(send_overloads, UDPMultiCast::sendWithTimeStamp, 1, 2);
 BOOST_PYTHON_MODULE(UDPClient_python)
 {
+	// alternative for getData etc would be to use:
+	// http://stackoverflow.com/questions/13675409/creating-python-collections-namedtuple-from-c-using-boostpython
+	// maybe we can do a bit better when we learn from <boost/python/tuple.hpp>
+	// may need this:
+	// http://stackoverflow.com/questions/2135457/how-to-write-a-wrapper-over-functions-and-member-functions-that-executes-some-co
+
 	// need to expose our message struct (NB: boost.python doesn't do plain arrays, convert to string for an easy fix)
 	class_<message>("message")
 		.add_property("text", static_cast<std::string(*)(const message&)>([](const message& m) {return std::string(m.text); }))	// casting to function pointer the ugly way as unary plus does not seems to work with VS2015
