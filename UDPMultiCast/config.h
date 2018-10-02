bc9a608d2a730ae87e8f93aa49b076d355f6ce5d
Diederick Niehorster <dcnieho@gmail.com>
2018-10-02 14:19:57 +0200

    force off SMI integration so that user does not need to have it installed. Better solution would be to dynamic-load the relevant functionality so we can compile it in and decide not to use it at runtime. This suffices for now

--- UDPMultiCast/config.h
+++ UDPMultiCast/config.h
@@ -2,14 +2,16 @@
 
 //#define IP_ADDR_AS_STR
 
-#if __has_include("iViewXAPI.h")
+// use the 0 or 1 at the end of the each below statement to forcefully switch off inclusion of certain functionality in the compilation
+
+#if __has_include("iViewXAPI.h") && 0
 #   define HAS_SMI_INTEGRATION
 #endif
 
-#if __has_include("G_User.h")
+#if __has_include("G_User.h") && 1
 #   define HAS_WINDOWSTIMESTAMPPROJECT
 #endif
 
-#if __has_include(<tobii_research.h>)
+#if __has_include(<tobii_research.h>) && 1
 #   define HAS_TOBII_INTEGRATION
 #endif
